d519629719ce5dfc30bf4780ca06fd62f57c7ac8
Ricardo SIGNES <rjbs@cpan.org>
2008-12-17 09:11:28 -0500

    call out bug in docs

--- lib/Data/OptList.pm
+++ lib/Data/OptList.pm
@@ -67,7 +67,9 @@ Just look at all those undefs!  Don't worry, we can get rid of those:
   ];
 
 Aaaauuugh!  We've saved a little typing, but now it requires thought to read,
-and thinking is even worse than typing.
+and thinking is even worse than typing... and it's got a bug!  It looked right,
+didn't it?  Well, the C<< xyz => { ... } >> gets consumed by the map, and we
+don't get the data we wanted.
 
 With Data::OptList, you can do this instead:
 
